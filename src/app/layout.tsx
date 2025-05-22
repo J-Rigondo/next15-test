0a80870eb245447d2eb1dfcb916df0aa79f4ab4f
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-22 23:58:52 +0900

    provider 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Noto_Sans_KR } from 'next/font/google';
+import AppProvider from '@/app-layer/app-provider';
 
 const notoSansKR = Noto_Sans_KR({
   weight: ['300', '400', '500', '600', '700', '800', '900'],
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={notoSansKR.className}>{children}</body>
+      <body className={notoSansKR.className}>
+        <AppProvider>{children}</AppProvider>
+      </body>
     </html>
   );
 }
