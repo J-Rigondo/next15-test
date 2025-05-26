cda1aaed316eb39b9cecfc83184b3be53d7894ec
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-26 15:34:19 +0900

    posts loading skeleton

---
+++ src/app/(public)/posts/loading.tsx
@@ -0,0 +1,19 @@
+export default function Loading() {
+  return (
+    <div className=" divide-y divide-slate-300 space-y-10">
+      {Array.from({ length: 10 }).map((_, i) => (
+        <div
+          key={i}
+          className="flex-shrink-0 animate-pulse flex pb-10 first:pt-10"
+        >
+          <div className="rounded-md w-[250px] h-[130px] bg-slate-200" />
+          <div className="space-y-2.5 pl-6">
+            <div className="h-4 rounded w-[250px] bg-slate-200" />
+            <div className="h-5 rounded w-[500px] bg-slate-200" />
+            <div className="h-3.5 rounded w-[300px] bg-slate-200" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
