890e8f4ed364646287d0da0c23ba5d5d2bd8ff77
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-26 22:00:19 +0900

    post 조회, 작성 서버 액션

---
+++ src/entities/post/api/post-action.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import prisma from '@/shared/lib/prisma';
+import { CreatePostDto } from '@/entities/post/api/types';
+import { getUserSessionId } from '@/shared/lib/server-session';
+
+export async function getPosts() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return prisma.post.findMany({
+    orderBy: { createdAt: 'desc' },
+    include: { author: { select: { username: true } } },
+  });
+}
+
+export async function createPost(request: CreatePostDto) {
+  const { type, title, content, isPublished } = request;
+  const sessionId = await getUserSessionId();
+  if (!sessionId) {
+    throw new Error('No sessionId provided');
+  }
+
+  await prisma.post.create({
+    data: {
+      type,
+      title,
+      content,
+      published: isPublished,
+      author: {
+        connect: {
+          session: sessionId,
+        },
+      },
+    },
+  });
+}
