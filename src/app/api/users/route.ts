f7ccc24089081f47e81264c6424a407aa24382e1
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-26 14:27:29 +0900

    유저명 랜덤 생성

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,15 +1,27 @@
 import prisma from '@/shared/lib/prisma';
 import { NextResponse } from 'next/server';
-import { setUserSessionId } from '@/shared/lib/server-session';
+import { randomUUID } from 'node:crypto';
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  animals,
+} from 'unique-names-generator';
 
 export async function POST(request: Request) {
-  const sessionId = await setUserSessionId();
+  const sessionId = randomUUID();
+  const username = uniqueNamesGenerator({
+    dictionaries: [adjectives, animals],
+    separator: '',
+    style: 'capital',
+    length: 2,
+  });
 
   await prisma.user.create({
     data: {
+      username,
       session: sessionId,
     },
   });
 
-  return NextResponse.json('ok');
+  return NextResponse.json({ sessionId });
 }
