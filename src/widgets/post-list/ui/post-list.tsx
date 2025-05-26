7e2aac0997cb56769822bb15e803292c249ce8a7
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-26 22:01:14 +0900

    post list ssr init

--- src/widgets/post-list/ui/post-list.tsx
+++ src/widgets/post-list/ui/post-list.tsx
@@ -1,20 +1,21 @@
 import { getPosts } from '@/entities/post/api/post-action';
 import PostCard from '@/entities/post/ui/post-card';
 import dayjs from 'dayjs';
+import { SAMPLE_IMAGES } from '@/widgets/post-list/lib/constant';
 
 export default async function PostList() {
   const posts = await getPosts();
 
   return (
     <div className=" divide-y divide-slate-300 space-y-10">
-      {posts.map((p) => (
+      {posts.map((p, idx) => (
         <PostCard
           key={p.id}
           postType={p.type}
           author={p.author.username}
           title={p.title}
           createdAt={dayjs(p.createdAt).format('YYYY-MM-DD HH:mm:ss')}
-          imgUrl="https://www.blockmedia.co.kr/wp-content/uploads/2024/10/%EA%B1%B0%EB%9E%98%EC%86%8C-%EC%A7%80%EA%B0%91-560x318.png"
+          imgUrl={SAMPLE_IMAGES[idx % SAMPLE_IMAGES.length]}
         />
       ))}
     </div>
