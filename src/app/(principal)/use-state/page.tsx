1c09b20e3d53176d9ec0869729e8abae043e06ed
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-26 22:00:27 +0900

    정리

--- src/app/(principal)/use-state/page.tsx
+++
@@ -1,9 +0,0 @@
-import { StateChanger } from '@/widgets/state-changer/ui/state-changer';
-
-export default function UseStatePage() {
-  return (
-    <div className="px-40 py-60">
-      <StateChanger />
-    </div>
-  );
-}
