364127f1dc17cdeefc4ad2319d58e27b4ebcb1b5
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-26 15:58:54 +0900

    post card 정의

---
+++ src/entities/post/ui/post-card.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+
+type PostCardProps = {
+  imgUrl: string;
+  postType: string;
+  title: string;
+  author: string;
+  createdAt: string;
+};
+
+export default function PostCard({
+  imgUrl,
+  postType,
+  title,
+  author,
+  createdAt,
+}: PostCardProps) {
+  return (
+    <div className="flex pb-10 first:pt-10">
+      <div className="flex-shrink-0 rounded-md w-[250px] h-[130px] relative overflow-hidden">
+        <Image className="object-cover" src={imgUrl} alt="Post Image" fill />
+      </div>
+      <div className="space-y-2.5 pl-6">
+        <p className="text-sm text-sky-700 font-medium">{postType}</p>
+        <p className="text-xl font-semibold">{title}</p>
+        <div className="flex items-center text-xs">
+          <p className=" text-gray-600 font-semibold mr-3">{author}</p>
+          <p className="text-gray-400">{createdAt}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
