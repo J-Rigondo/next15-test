9bc1edb7efee065fa7d8c48286fbea8165297d2d
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-26 22:01:39 +0900

    use state test txt

---
+++ src/app/(public)/(principal)/use-state/page.tsx
@@ -0,0 +1,13 @@
+import { StateChanger } from '@/widgets/state-changer/ui/state-changer';
+
+export default function UseStatePage() {
+  return (
+    <div className="p-20">
+      <h1 className="text-xl font-bold text-gray-900 my-6">
+        UseState initialState에 props를 넣을 경우 리렌더링과 상관없이 단 한번만
+        초기화 된다.
+      </h1>
+      <StateChanger />
+    </div>
+  );
+}
