4b0721d07cbc897e4edce4b0f809e6523e726265
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-26 15:34:26 +0900

    error page sample

---
+++ src/app/(public)/posts/error.tsx
@@ -0,0 +1,31 @@
+'use client'; // Error components must be Client Components
+
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <h2 className="mb-5 text-2xl">Something went wrong!</h2>
+      <button
+        className="rounded-md bg-red-600 p-4 font-medium text-white"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
