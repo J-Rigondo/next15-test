8a24a5bbacb1a8d8132270cf8c41c64da6411852
Jun Hyeong Park <futuregoing46@gmail.com>
2025-05-26 16:41:16 +0900

    이미지 domain 허용

---
+++ src/widgets/post-list/lib/constant.ts
@@ -0,0 +1,5 @@
+export const SAMPLE_IMAGES = [
+  'https://images.unsplash.com/photo-1682688759350-050208b1211c?q=60&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1613169620329-6785c004d900?q=60&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1725025422959-4d245b3430bf?q=60&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+];
