a884c399b5e418632a079e782ab08d67eefb17d9
Jun Hyeong Park <futuregoing@prismweaver.com>
2025-06-04 17:02:45 +0900

    ISR은 fetch가 있어야 빌드 때 적용이된다.

--- src/app/[locale]/(public)/posts/page.tsx
+++ src/app/[locale]/(public)/posts/page.tsx
@@ -1,6 +1,11 @@
 import { PostList } from '@/widgets/post-list';
+import { routing } from '@/i18n/routing';
 
 export const revalidate = 60;
+export const dynamicParams = true;
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
 
 export default async function PostPage() {
   const data = await fetch('https://api.vercel.app/blog');
